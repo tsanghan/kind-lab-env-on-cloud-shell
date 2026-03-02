c123fa2c4ac63594715385d3ab46384fa98844be
tsanghan <tsanghan@gmail.com>
2026-03-02 18:01:48 +0800

    added export to PATH

--- setup.sh
+++ setup.sh
@@ -165,7 +165,7 @@ cat <<'EOF' >>"$HOME/.bashrc"
 
 # set PATH so it includes user's private bin if it exists
 if [ -d "$HOME/.local/bin" ] ; then
-    PATH="$HOME/.local/bin:$PATH"
+    export PATH="$HOME/.local/bin:$PATH"
 fi
 EOF
 
