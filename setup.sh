641dea098bb4e62c66fc2c24cf174094375102e9
tsanghan <tsanghan@gmail.com>
2026-03-02 18:04:29 +0800

    added full path to docker-network-kind.sh

--- setup.sh
+++ setup.sh
@@ -197,4 +197,4 @@ envsubst '$DH_NAMESPACE:$KINDEST_NODE_VER' < projects-kind/kind.yaml | tee ~/Pro
 envsubst '$CERT_MANAGER_VER' < ./local-bin/cert-manager.sh > ~/.local/bin/cert-manager.sh
 chmod +x ~/.local/bin/cert-manager.sh
 
-docker-network-kind.sh
+"$HOME/.local/bin/docker-network-kind.sh"
